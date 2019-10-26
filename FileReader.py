49dc8fe78ccd1e32db0d277993549151e825ab34
hurry Hurchand <hhurchand>
2019-10-26 00:24:57 -0400

    Update FileReader.py

--- FileReader.py
+++ FileReader.py
@@ -2,15 +2,19 @@
 
 Script
 (i) reads house.data file
-(ii)prints each line of the file
+(ii)prints each line of the file - part needs to be uncommented
 (iii) splits each line and prints it as an array
 
 '''
 
 f = open(file="housing.data")
 
+"""Don't use readline(), as line in already acts as a read!"""
 for line in f:
-    b = f.readline()
-    print(b.split())
+
+    """Uncomment next line to print each line of the file"""
+    """print(line)"""
+
+    print(line.split())
 
 f.close()
